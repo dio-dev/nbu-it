0775894a5223b367e58c2dc2a86c2ba50cae1c83
Andrii <andreybukhanenko@gmail.com>
2019-10-27 20:04:29 +0200

    Authenticating a Flask API

--- app/view.py
+++ app/view.py
@@ -1,12 +1,23 @@
 import pymysql
 from config import mysql
 from app import app,api
-from flask import jsonify
+from flask import jsonify, make_response
 from flask_restful import Resource
+from flask_httpauth import HTTPBasicAuth
+auth = HTTPBasicAuth()
 
+@auth.get_password
+def get_password(username):
+    if username == 'qwerty':
+        return 'qwerty'
+    return None
 
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
 
 class BankWatchYou(Resource):
+    @auth.login_required
     def get(self):
         try:
             conn = mysql.connect()
