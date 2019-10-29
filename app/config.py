d2884bf27f9b5a249f2960566643f56f159db2d3
Andrii <andreybukhanenko@gmail.com>
2019-10-29 22:23:05 +0200

    add mysql config to .env

--- app/config.py
+++ app/config.py
@@ -1,10 +1,19 @@
+import os
 from app import app
 from flaskext.mysql import MySQL
+from dotenv import load_dotenv
+load_dotenv()
+from pathlib import Path  # python3 only
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
 mysql = MySQL()
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = 'worker'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'TXWs^18#Nd17'
-app.config['MYSQL_DATABASE_DB'] = 'hackathon'
-app.config['MYSQL_DATABASE_HOST'] = '176.36.240.68'
+app.config['MYSQL_DATABASE_USER'] = os.getenv("MYSQL_DATABASE_USER")
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv("MYSQL_DATABASE_PASSWORD")
+app.config['MYSQL_DATABASE_DB'] = os.getenv("MYSQL_DATABASE_DB")
+app.config['MYSQL_DATABASE_HOST'] = os.getenv("MYSQL_DATABASE_HOST")
+
 mysql.init_app(app)
